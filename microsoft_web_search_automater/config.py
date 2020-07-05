aee3a229237f7607980947fe52cb813a9ea1b359
Aakhash Ganesh <aakhashgv@gmail.com>
2020-07-05 14:41:13 -0400

    Delete config.py

--- microsoft_web_search_automater/config.py
+++
@@ -1,6 +0,0 @@
-class info ():
-    username = 'aakhashgv@gmail.com'
-    password = 'Hsahkaavg0!'
-    message = "Hey, I'm just testing a program that I wrote in like 20 minutes and I just needed it to DM a bunch of people. Hope your summer is going great!"
-    usernames = ['dramashotz', 'lanrey_1', 'xelandrica', '_daniellerachelle_', 'kiptouseull', 'lawuest', 'richter_brad', '_theycallmedirtydan_']
-    path = "/home/jagelta/Developement/webdriver/geckodriver"
